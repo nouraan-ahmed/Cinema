4e314ec730c9716c47361d6f995d965c2b709505
boules <boles100@yahoo.com>
2020-02-11 21:55:02 +0200

    b

--- Cinema-master/shop/js/shop.js
+++ Cinema-master/shop/js/shop.js
@@ -1,10 +1,20 @@
-document.querySelectorAll('.card a').addEventListener('click',
-function() {
-    document.querySelector('.bg-modal').style.display = 'flex';
-}
+var elements = document.querySelectorAll(".card");
+
+for (let i =0; i< elements.length; i++ ){
+elements[i].addEventListener('click',
+    function() {
+        document.querySelector('.bg-modal').style.display = 'flex';
+    }
 );
+}
 
 document.querySelector('.close').addEventListener('click',
+    function() {
+        document.querySelector('.bg-modal').style.display= 'none';
+    }
+)
+
+document.querySelector('#cancel').addEventListener('click',
     function() {
         document.querySelector('.bg-modal').style.display= 'none';
     }
