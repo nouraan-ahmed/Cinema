0fc99f372b7e105c8c9f670e64f7d80cd2c6365a
nouraan-ahmed <norahahmed1999@gmail.com>
2021-01-14 14:05:42 +0200

    Revert "l8"
    This reverts commit c25cbf54f753015d177a3296ecb9dc176759a3df.

---
+++ Cinema-master/sign in/js/first.js
@@ -0,0 +1,22 @@
+	$(document).ready(function()
+{
+	// $(".input").animate({
+	// 	top:'60%',
+
+
+	// }, 2000,);
+
+
+
+		$(".input").animate({
+		width:'580px',
+		height:'490px',
+		borderRadius:'10px',
+
+
+	}, 2000 , function(){$('.n').fadeIn(1000)});
+
+})
+
+
+
